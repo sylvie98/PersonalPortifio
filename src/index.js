d3a9427ee4c8daac080b374283d3e9b8435083d0
sylvie98 <sylviemasengesho5@gmail.com>
2024-03-17 10:50:53 +0200

    four commit

---
+++ src/index.js
@@ -0,0 +1,6 @@
+const burgerBtn = document.getElementById('burgerBtn');
+const mobileMenu = document.getElementById('mobileMenu');
+
+burgerBtn.addEventListener('click', () => {
+  mobileMenu.classList.toggle('hidden');
+});
